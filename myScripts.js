9ea57e0943a1e706620a77f3affc11b39ecbc0d4
alubeariskool <39659741+alubeariskool@users.noreply.github.com>
2020-01-24 13:28:15 -0500

    Create myScripts.js

---
+++ myScripts.js
@@ -0,0 +1,23 @@
+function myLoop(){
+var start = document.getElementById("start").value;
+var end = document.getElementById("end").value;
+var jump = document.getElementById("jump").value;
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  start = Number(start)
+  end = Number(end)
+  jump = Number(jump)
+  
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(typeof jump);
+  
+  for(var i = start; i <= end; i+=jump)
+  {
+    console.log(i);
+    document.write('<p>' + i + '</p>');
+  }
+}
